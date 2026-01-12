f7a1bd0a8af4bc3aa2b97db73f1fa9f7825b18ac
Techbitforge <techbitforgeoffice.com@gmail.com>
2026-01-12 20:08:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,3 +14,20 @@ if [ -z "$SSH_PASS" ]; then
     exit 1
 fi
 echo "$SSH_USER:$SSH_PASS" | chpasswd
+
+# Add the user to sudo group so you can use 'sudo' commands
+usermod -aG sudo "$SSH_USER"
+#2. START SERVICES
+# Start the Fake Flask API in the background
+echo "Starting Flask APΙ..."
+python3 /app/app.py &
+# Start the SSH Server
+echo "Starting SSH Server..."
+service ssh start
+# Start the Cloudflare Tunnel
+if [ -z "$CF_TOKEN" ]; then
+echo "Error: CF_TOKEN secret is missing!"
+exit 1
+fi
+echo "Starting Cloudflare Tunnel..."
+cloudflared tunnel run --token "$CF_TOKEN"
